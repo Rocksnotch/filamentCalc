57d7ee4c447addad33038ccdfa061d4d623c40fc
rocksnotch <williamyouse8@gmail.com>
2023-01-24 16:24:01 -0500

    Create main.cpp
    main.cpp created

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+
+void header() {
+    system("clr");
+    std::cout << "==========CSO Printer Calculator V0.1==========" << std::endl << std::endl << std::endl;
+}
+
+int main() {
+    header();
+    return 0;
+}
