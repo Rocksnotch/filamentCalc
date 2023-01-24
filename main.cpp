1b01824527ad003aaa2826c2b2bf4c7a75e08dd8
rocksnotch <williamyouse8@gmail.com>
2023-01-24 16:29:53 -0500

    Update main.cpp
    Implimended header function and screen clearing function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <stdlib.h>
 
+void clear_screen()
+{
+#ifdef _WIN32
+    std::system("cls");
+#else
+    // Assume POSIX
+    std::system ("clear");
+#endif
+}
+
 void header() {
-    system("clr");
+    clear_screen();
     std::cout << "==========CSO Printer Calculator V0.1==========" << std::endl << std::endl << std::endl;
 }
 
