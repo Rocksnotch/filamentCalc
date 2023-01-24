7327f60970c9247c8e6f6bafda15dbffec4d6e2c
rocksnotch <williamyouse8@gmail.com>
2023-01-24 16:46:53 -0500

    Update main.cpp
    main.cpp now contains checks for a 'config.dat' file, and generates a false one for testing at the moment.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
+
 
 void clear_screen()
 {
@@ -13,10 +15,30 @@ void clear_screen()
 
 void header() {
     clear_screen();
-    std::cout << "==========CSO Printer Calculator V0.1==========" << std::endl << std::endl << std::endl;
+    std::cout << "==========CSO Printer Calculator V0.1==========" << std::endl << std::endl;
+}
+
+void configCheck() {
+    std::fstream cFile;
+    cFile.open("config.dat");
+
+    if (cFile)
+    {
+        std::cout << "Config found, loading config file." << std::endl;
+    } else {
+        std::cout << "Config not found, creating config file..." << std::endl;
+        cFile << "";
+        std::cout << "Done!\nPress 'Enter' to continue";
+        std::cin.get();
+        header();
+    }
+    
 }
 
 int main() {
     header();
+    configCheck();
+
+
     return 0;
 }
