6b483696764c0c0fa23368345ca2f9c49b9faa57
rocksnotch <williamyouse8@gmail.com>
2023-01-25 10:09:21 -0500

    updated main.cpp
    +config saving
    +config loading
    
    Testing concluded that the function works with ONE input. Will test later when easier addition function is added.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <string>
 #include <stdlib.h>
 #include <fstream>
+#include <vector>
 
 // Unloaded variables
 std::string spoolBrand = "";
 std::string spoolType = "";
 double spoolCost = 0.0;
-int spoolWeight = 0;
+double spoolWeight = 0;
 
 // Loaded variables
 const double pla = 1.24;
-const double abs = 1.04;
 
 
 void clear_screen()
@@ -51,14 +52,77 @@ void configCheck() {
 struct filament {
         
         std::string brand;
+        std::string type;
         double cost;    
+        double weight;
 
-    };
+    } hold;
+
+// Vector Setup
+std::vector<filament> fil;
+
+void configSave() {
+    header();
+    std::cout << "Saving config..." << std::endl;
+    std::ofstream wFile;
+    wFile.open("config.dat");
+
+    int vecSize = 0;
+    vecSize = fil.size();
+
+    for (auto& a : fil) {
+        wFile << a.brand << std::endl;
+        wFile << a.type << std::endl;
+        wFile << a.cost << std::endl;
+        wFile << a.weight << std::endl;
+    }
+
+    wFile.close();
+    std::cout << "Config saved!\nPress 'Enter' to continue" << std::endl;
+    std::cin.get();
+    header();
+}
+
+void configLoad() {
+    header();
+    std::cout << "Loading config..." << std::endl;
+    std::ifstream rFile;
+    rFile.open("config.dat");
+
+    while (!rFile.eof()) {
+        std::getline(rFile, hold.brand);
+        std::getline(rFile, hold.type);
+        rFile >> hold.cost;
+        rFile >> hold.weight;
+
+        fil.push_back(hold);        
+    }
+
+    rFile.close();
+    std::cout << "Config loaded!\nPress 'Enter' to continue" << std::endl;
+    std::cin.get();
+    header();
+}
 
 int main() {
     header();
     configCheck();
+/*
+    struct filament test;
+    test.brand = "testBrand";
+    test.type = "pla";
+    test.cost = 29.99;
+    test.weight = 15;
 
+    fil.push_back(test);
+*/
+
+    configLoad();
+
+    std::cout << fil[0].brand << std::endl;
+    std::cout << fil[0].type << std::endl;
+    std::cout << fil[0].cost << std::endl;
+    std::cout << fil[0].weight << std::endl;
 
     return 0;
 }
